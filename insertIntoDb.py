264e678e08a37082d5b7a1b6363975dda13054e7
srameshv <srameshv@indiana.edu>
2017-04-25 11:53:48 -0400

    tfidf feature extraction

--- insertIntoDb.py
+++ insertIntoDb.py
@@ -1,34 +1,35 @@
 from pymongo import MongoClient
 import os
 
+
 def insert():
-	client = MongoClient('localhost:27017', serverSelectionTimeoutMS=1000)
+    client = MongoClient('localhost:27017', serverSelectionTimeoutMS=1000)
 
-	db = client['user-details']
-	collection = db['status']
+    db = client['user-details']
+    collection = db['status']
 
-	os.chdir(os.path.join(os.getcwd(),'processed-data'))
+    os.chdir(os.path.join(os.getcwd(), 'processed-data'))
 
-	folders = os.listdir()
+    folders = os.listdir()
 
-	s_no = 1
+    s_no = 1
 
-	for folder in folders:
-		temp = folder.split('-')
-		if len(temp) > 2:
-			for status in os.listdir(folder):
-				file = open(folder+'/'+status,'r')
-				status_content = file.read()
-				file.close()
-				status = status.split('.')[0]
-				result = db.collection.insert_one(
-					{
-					"SNo": s_no,
-					"StatusID" : status_content,
-					"Status" : status,
-					"Age" : temp[1]+'-'+temp[2],
-					"Sex" : temp[0]
-					})
-				print(s_no)
-				s_no += 1
-	client.close()
+    for folder in folders:
+        temp = folder.split('-')
+        if len(temp) > 2:
+            for status in os.listdir(folder):
+                file = open(folder + '/' + status, 'r')
+                status_content = file.read()
+                file.close()
+                status = status.split('.')[0]
+                result = db.collection.insert_one(
+                    {
+                        "SNo": s_no,
+                        "StatusID": status_content,
+                        "Status": status,
+                        "Age": temp[1] + '-' + temp[2],
+                        "Sex": temp[0]
+                    })
+                print(s_no)
+                s_no += 1
+    client.close()
--- profiling.py
+++ profiling.py
@@ -1,6 +1,10 @@
 from pymongo import MongoClient
 from insertIntoDb import insert
 from getTweets import lookupTweets
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+import random
 
 client = MongoClient('localhost:27017', serverSelectionTimeoutMS=1000)
 
@@ -8,7 +12,24 @@ db = client['user-details']
 collection = db['status']
 
 if db.collection.count() < lookupTweets():
-	db.collection.remove()
-	insert()
+    db.collection.remove()
+    insert()
 else:
-	print("Hello world", lookupTweets())
+    print("Hello world", lookupTweets())
+    # Partition in to Training and Test Set.
+    data = [db["StatusID"]+"-"+db["Sex"] for db in db.collection.find()]
+    random.shuffle(list(data))
+    training_data = data[:int((len(data)+1)*.80)]
+    test_data = data[int(len(data)*.80+1):]
+
+    # Tokenizing & Filtering the text
+    count_vectorizer = CountVectorizer()
+    X_train_counts = count_vectorizer.fit_transform(training_data)
+
+    # Fit and Transform the data
+    tfidf_transformer = TfidfTransformer()
+    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+
+    # Train the classifer using various methods
+
+
