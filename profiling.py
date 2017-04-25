93ad46ec8c6f6b271658fff434adcdf174a1f34f
srameshv <srameshv@indiana.edu>
2017-04-25 12:45:05 -0400

    Changed the feature extracion

--- profiling.py
+++ profiling.py
@@ -1,9 +1,12 @@
 from pymongo import MongoClient
 from insertIntoDb import insert
 from getTweets import lookupTweets
+from sklearn import svm
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
 import random
 
 client = MongoClient('localhost:27017', serverSelectionTimeoutMS=1000)
@@ -16,20 +19,38 @@ if db.collection.count() < lookupTweets():
     insert()
 else:
     print("Hello world", lookupTweets())
-    # Partition in to Training and Test Set.
-    data = [db["StatusID"]+"-"+db["Sex"] for db in db.collection.find()]
-    random.shuffle(list(data))
-    training_data = data[:int((len(data)+1)*.80)]
+    # Partition in to Training and Test Set for gender.
+    data = [db["StatusID"]+"---"+db["Sex"] for db in db.collection.find()]
+    random.shuffle(data)
+    train_data = data[:int((len(data)+1)*.80)]
     test_data = data[int(len(data)*.80+1):]
+    train_labels = [data.split("---")[1] for data in train_data]
+    test_labels = [data.split("---")[1] for data in test_data]
 
     # Tokenizing & Filtering the text
-    count_vectorizer = CountVectorizer()
-    X_train_counts = count_vectorizer.fit_transform(training_data)
+    # min_df=5, discard words appearing in less than 5 documents
+    # max_df=0.8, discard words appering in more than 80% of the documents
+    # sublinear_tf=True, use sublinear weighting
+    # use_idf=True, enable IDF
 
-    # Fit and Transform the data
-    tfidf_transformer = TfidfTransformer()
-    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+    vectorizer = TfidfVectorizer(min_df=3,
+                             max_df = 0.8,
+                             sublinear_tf=True,
+                             use_idf=True)
+    train_vectors = vectorizer.fit_transform(train_data) # create the vocabulary and the feature weights from the training data
+    test_vectors = vectorizer.transform(test_data) # create the feature weights for the test data
 
-    # Train the classifer using various methods
+    # Classification using SVM kernel=rbf
+    # classifier_rbf = svm.SVC()
+    # classifier_rbf.fit(train_vectors, train_labels)
+    # prediction_rbf = classifier_rbf.predict(test_vectors)
+    # print(classification_report(test_labels, prediction_rbf))
+
+
+    # # Fit and Transform the data
+    # # tfidf_transformer = TfidfTransformer()
+    # # X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+
+    # # Train the classifer using various methods
 
 
